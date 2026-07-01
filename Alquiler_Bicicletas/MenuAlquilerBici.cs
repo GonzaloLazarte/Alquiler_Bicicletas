536d2e9d254b2e8d10b42919e4c451344818a54a
Gonzalo_Lazarte <lazarte.gonzalo7000@gmail.com>
2026-07-01 20:41:46 -0300

    se_añade_opcion_de_salida

--- Alquiler_Bicicletas/MenuAlquilerBici.cs
+++ Alquiler_Bicicletas/MenuAlquilerBici.cs
@@ -8,24 +8,30 @@ namespace Alquiler_Bicicletas {
     internal class MenuAlquilerBici {
         static AlquilerBicicletas servicio = new AlquilerBicicletas();  //instanciamos un objeto servicio para acceder a las propiedades de la clase alquilerBicicletas
         public void EjecutarMenu() {
-            servicio.EjecutarServicioAlquiler(); // ejecuta el metodo del objeto servicio que inicializa el archivo de bicicletas
+            servicio.EjecutarServicioAlquiler();                        // ejecuta el metodo del objeto servicio que inicializa el archivo de bicicletas
             int opcion = 0;
-            while (true) {
+            bool bucle = true;
+            while (bucle) {
                 Console.Clear();
                 Console.WriteLine("============================== ALQUILER DE BICICLETAS ==============================");
                 Console.WriteLine("================================== MENU DE OPCIONES ================================");
                 Console.WriteLine("");
                 Console.WriteLine("1) - RETIRAR BICICLETA");
+                Console.WriteLine("");
                 Console.WriteLine("2) - VER BICICLETAS");
+                Console.WriteLine("");
                 Console.WriteLine("3) - DEVOLVER BICICLETA");
+                Console.WriteLine("");
+                Console.WriteLine("4) - SALIR DEL PROGRAMA");
                 Console.WriteLine("");
                 Console.WriteLine("====================================================================================");
                 Console.Write("PORFAVOR SELECCIONE UNA OPCION: > ");
-                if (int.TryParse(Console.ReadLine(), out opcion) && opcion >= 1 && opcion <= 3) {    //de validacion de la opcion ingresada
+                if (int.TryParse(Console.ReadLine(), out opcion) && opcion >= 1 && opcion <= 4) {    //de validacion de la opcion ingresada
                     switch (opcion) {
                         case 1: servicio.Retirar(); break;              // 
                         case 2: servicio.Mostrar(); break;              //ejecutan los respectivos metodos del objeto servicio segun la opcion seleccionada
                         case 3: servicio.Devolver(); break;             //
+                        case 4: bucle = false; break;
                     }
                 }
                 else {
@@ -33,6 +39,11 @@ namespace Alquiler_Bicicletas {
                     Console.ReadKey();
                 }
             }
+            Console.Clear();
+            Console.WriteLine("====================================================================================");
+            Console.WriteLine("||             GRACIAS POR USAR NUESTRO PROGRAMA HASTA LA PROXIMA                 ||");
+            Console.WriteLine("====================================================================================");
+            //Console.ReadKey();
         }
     }
 }
